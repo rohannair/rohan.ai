adfb0ec2088a14c6fd8653bbbc438dc5fbdaf2b4
Rohan Nair <r@rohannair.ca>
2019-11-29 15:42:56 -0500

    Update deploy settings

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Logo from './Logo'
 import _Wrapper from './Wrapper'
 
